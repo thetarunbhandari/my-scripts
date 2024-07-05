4f4ecaa72962860ac335fae7a3aa382c9796fe63
thetarunbhandari <thetarunbhandari11@gmail.com>
2024-07-06 02:00:43 +0530

    add critical option

--- notification-scripts/battery-notify.sh
+++ notification-scripts/battery-notify.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 low=20
-full=95
+full=98
 
 battery_now=$(cat /sys/class/power_supply/BAT0/capacity)
 battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
 if [ "$battery_now" -lt $low ] && [ "$battery_status" != "Charging" ]; then
-	notify-send -i "~/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "Charging Low. Please connect to the charger"
+	notify-send -u critical -i "~/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "Charging Low. Please connect to the charger"
 elif [ "$battery_now" -eq $full ] && [ "$battery_status" == "Charging" ]; then
-	notify-send  -i "~/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "System is fully charged. Please Unplug the charger"
+	notify-send -u critical -i "~/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "System is fully charged. Please Unplug the charger"
 else
 	exit
 fi
