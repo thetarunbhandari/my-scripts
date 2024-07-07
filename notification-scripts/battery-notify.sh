5b33c6ed46ea0a5ecaa656bfe3612760d44c3489
thetarunbhandari <thetarunbhandari11@gmail.com>
2024-07-07 17:39:25 +0530

    modify
    add the while loop which will infintely perform the task

--- notification-scripts/battery-notify.sh
+++ notification-scripts/battery-notify.sh
@@ -3,14 +3,17 @@
 low=20
 full=99
 
-battery_now=$(cat /sys/class/power_supply/BAT0/capacity)
-battery_status=$(cat /sys/class/power_supply/BAT0/status)
+while true; do
+    battery_now=$(cat /sys/class/power_supply/BAT0/capacity)
+    battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
-if [ "$battery_now" -lt $low ] && [ "$battery_status" != "Charging" ]; then
-	notify-send -u critical -i "~/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "Charging Low. Please connect to the charger"
-elif [ "$battery_now" -eq $full ] && [ "$battery_status" == "Charging" ]; then
+    if [ "$battery_now" -lt $low ] && [ "$battery_status" != "Charging" ]; then
+       notify-send -u critical -i "~/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "Charging Low. Please connect to the charger"
+       sleep 1m
+    elif [ "$battery_now" -eq $full ] && [ "$battery_status" == "Charging" ]; then
 	notify-send -u critical -i "~/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "System is fully charged. Please Unplug the charger"
-else
-	exit
-fi
-
+	sleep 1m
+    else
+	sleep 1m
+    fi
+done
