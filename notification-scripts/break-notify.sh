9f1a7affb5b7466d4a1c894ce79aa898d9d00d9d
thetarunbhandari <thetarunbhandari11@gmail.com>
2024-07-13 15:51:03 +0530

    add break-notify.sh
    a script which will tell me to take break

---
+++ notification-scripts/break-notify.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+NORMAL_BREAK_TIME="1.5 hours"
+MID_BREAK_TIME="2 hours"
+HIGH_BREAK_TIME='2.5 hours'
+
+while true; do
+    # get the running time
+    uptime="`uptime -p | sed -e 's/up //g'`"
+    echo $uptime
+
+    if [[ $uptime == $NORMAL_BREAK_TIME ]]; then
+        notify-send -u critical -i "~/Dev/scripts/notification-scripts/icons/system.svg" "System" "It's time to take some break"
+        sleep 5
+    fi
+    if [[ $uptime == $MID_BREAK_TIME ]]; then
+        notify-send -u critical -i "~/Dev/scripts/notification-scripts/icons/system.svg" "System" "It's time to take some break"
+        sleep 5
+    fi
+    if [[ $uptime == $HIGH_BREAK_TIME ]]; then
+        notify-send -u critical -i "~/Dev/scripts/notification-scripts/icons/system.svg" "System" "It's been 2 hour. The system will lock in 5 second"
+        sleep 5
+        i3lock -i ~/Pictures/background/2.png && systemctl suspend
+    fi
+    sleep 1m
+done
