9fc2f579d68590e5d039495e554a7d26aad866c8
thetarunbhandari <thetarunbhandari11@gmail.com>
2024-07-19 02:24:34 +0530

    modify path of icons

--- notification-scripts/battery-notify.sh
+++ notification-scripts/battery-notify.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 low=20
-full=80
+full=95
 
 while true; do
     battery_now=$(cat /sys/class/power_supply/BAT0/capacity)
     battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
     if [ "$battery_now" -lt $low ] && [ "$battery_status" != "Charging" ]; then
-       notify-send -u critical -i "~/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "Charging Low. Please connect to the charger"
+       notify-send -u critical -i "$HOME/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "Charging Low. Please connect to the charger"
     elif [ "$battery_now" -eq $full ] && [ "$battery_status" == "Charging" ]; then
-	notify-send -u critical -i "~/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "System is fully charged. Please Unplug the charger"
+	notify-send -u critical -i "$HOME/Dev/scripts/notification-scripts/icons/bolt.svg" "System" "System is fully charged. Please Unplug the charger"
     fi
     sleep 1m
 done
