efccabfe4989573cf9e4c6d6dfd47b5e3f576f06
thetarunbhandari <thetarunbhandari11@gmail.com>
2024-07-26 19:14:12 +0530

    remove personal-project directory

--- make-dir.sh
+++ make-dir.sh
@@ -3,12 +3,6 @@
 # check whether the Dev directory exist or not
 if [[ -d "$HOME/Dev/" ]]; then
     echo "Dev direcory exist"
-    if [[ -d "$HOME/Dev/personal-projects/" ]];then
-        echo "personal-projects directory exist in $HOME/Dev/"
-    else
-        echo "Creating personal-projects directory in $HOME/Dev/"
-        mkdir "$HOME/Dev/personal-projects/"
-    fi
 else
     echo "Creating Dev directory in $HOME"
     mkdir "$HOME/Dev/"
